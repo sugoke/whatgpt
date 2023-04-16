5bccbb0b774780ce5e0cbd2e6210451d54825632
Michael <michael@Air-de-Maryia.lan>
2023-04-16 21:59:22 +0200

    Initial commit

--- server/main.js
+++ server/main.js
@@ -4,60 +4,29 @@ import Twilio from 'twilio';
 import fetch from 'node-fetch';
 import bodyParser from 'body-parser';
 
-
-
-
 console.log(Meteor.settings);
 
+const TWILIO_ACCOUNT_SID = Meteor.settings.private.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTH_TOKEN = Meteor.settings.private.TWILIO_AUTH_TOKEN;
+const MY_PHONE_NUMBER = Meteor.settings.private.TWILIO_PHONE_NUMBER;
+const AMAZON_AFFILIATE_TAG = Meteor.settings.private.AMAZON_AFFILIATE_TAG;
+const OPENAI_API_KEY = Meteor.settings.private.OPENAI_API_KEY;
 
-
-
-//console.log(process.env.TWILIO_ACCOUNT_SID);
-
-  const TWILIO_ACCOUNT_SID = Meteor.settings.private.TWILIO_ACCOUNT_SID;
-  const TWILIO_AUTH_TOKEN = Meteor.settings.private.TWILIO_AUTH_TOKEN;
-  const MY_PHONE_NUMBER = Meteor.settings.private.TWILIO_PHONE_NUMBER;
-  const AMAZON_AFFILIATE_TAG = Meteor.settings.private.AMAZON_AFFILIATE_TAG;
-  const OPENAI_API_KEY = Meteor.settings.private.OPENAI_API_KEY;
-
-  console.log('TWILIO_ACCOUNT_SID:', TWILIO_ACCOUNT_SID);
-  //console.log('TWILIO_AUTH_TOKEN:', TWILIO_AUTH_TOKEN);
-  //console.log('TWILIO_PHONE_NUMBER:', MY_PHONE_NUMBER);
-  //console.log('AMAZON_AFFILIATE_TAG:', AMAZON_AFFILIATE_TAG);
-  //console.log('OPENAI_API_KEY:', OPENAI_API_KEY);
-
-  // Your other startup code goes here...
-
-
-
-//console.log(meteorSettings.TWILIO_ACCOUNT_SID)
-
+console.log('TWILIO_ACCOUNT_SID:', TWILIO_ACCOUNT_SID);
 
 const twilioClient = Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-//console.log('From:', MY_PHONE_NUMBER);
-//console.log('To:', to);
-
 function sendText(to, body) {
-
   console.log(`Sending text to ${to}: ${body}`);
-
   return twilioClient.messages.create({
     from: MY_PHONE_NUMBER,
-    to:to,
+    to: to,
     body
   });
 }
 
-
 async function getChatGPTResponse(prompt) {
-//  const API_KEY = process.env.OPENAI_API_KEY;
-
-const response = await getChatGPTResponse(prompt);
-console.log('ChatGPT response:', response);
-
-const apiResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
-
+  const apiResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -72,11 +41,9 @@ const apiResponse = await fetch('https://api.openai.com/v1/engines/text-davinci-
     })
   });
 
-  const data = await response.json();
-
-console.log(data)
-
-  return data//.choices[0].text.trim();
+  const data = await apiResponse.json();
+  console.log(data);
+  return data;
 }
 
 const impersonationTargets = {};
@@ -85,27 +52,21 @@ async function processMessage(message) {
   const senderPhoneNumber = message.From;
   const text = message.Body;
 
-
-
   console.log(`Message received from ${senderPhoneNumber}: ${text}`);
 
-console.log(`Impersonation target for ${senderPhoneNumber}: ${person}`);
-
-  // Check if it's the first message
   if (text.toLowerCase().startsWith('impersonate')) {
     const person = text.slice(11).trim();
-    // Save the impersonation target in a user's session (or database)
     impersonationTargets[senderPhoneNumber] = person;
+    console.log(`Impersonation target for ${senderPhoneNumber}: ${person}`);
     await sendText(senderPhoneNumber, `I'm now impersonating ${person}. You can ask me questions as if I were them.`);
   } else {
-    // Get the impersonation target from the user's session (or database)
     const person = impersonationTargets[senderPhoneNumber] || 'Anonymous';
+    console.log(`Impersonation target for ${senderPhoneNumber}: ${person}`);
     const prompt = `As ${person}, ${text}`;
     const response = await getChatGPTResponse(prompt);
     await sendText(senderPhoneNumber, response);
   }
 }
-
 
 WebApp.connectHandlers.use(bodyParser.urlencoded({ extended: false }));
 
